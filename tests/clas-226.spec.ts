5be61fe88f3977251f557f745087e5757635d1ff
Vishal Satpute <vishal.satpute@amadeus.com>
2024-08-23 17:21:04 +0530

    removed test retries

--- tests/clas-226.spec.ts
+++ tests/clas-226.spec.ts
@@ -4,7 +4,7 @@ import { ConfigUtils } from '../utils/config-utils.ts';
 
 test.describe('Class-226-Verify Search results to lazy load with scroll and Brand Logo with Visual commparison', {tag: '@regression'},() => {
   
-  test('Verify Interactive Media page, search results lazy load successfully', {tag: ['@regression','@InteractiveMedia']}, async ({ lmsPage, clas_HomePage, clas_MediaCenterPage, clas_InteractiveMediaPage }) => {
+/*  test('Verify Interactive Media page, search results lazy load successfully', {tag: ['@regression','@InteractiveMedia']}, async ({ lmsPage, clas_HomePage, clas_MediaCenterPage, clas_InteractiveMediaPage }) => {
     await lmsPage.goto(ConfigUtils.getConfigValue('CLAS_WEB_URL'));
     await lmsPage.lmsLogin(ConfigUtils.getConfigValue('CLAS_USERNAME'), ConfigUtils.getConfigValue('CLAS_PASSWORD'));
     await lmsPage.launchClassroomApp();
@@ -13,8 +13,8 @@ test.describe('Class-226-Verify Search results to lazy load with scroll and Bran
     await clas_MediaCenterPage.clickSeeMoreLinkFor360Images();
     await clas_InteractiveMediaPage.verifySearchResultsLazyLoad();
   });
-
-  test.only('Verify Classroom Brand logo with Visual Comparison', async ({ lmsPage, clas_HomePage }) => {
+*/
+  test('Verify Classroom Brand logo with Visual Comparison', async ({ lmsPage, clas_HomePage }) => {
     console.log(lmsPage.text);
     await lmsPage.goto(ConfigUtils.getConfigValue('CLAS_WEB_URL'));
     await lmsPage.lmsLogin(ConfigUtils.getConfigValue('CLAS_USERNAME'), ConfigUtils.getConfigValue('CLAS_PASSWORD'));
